1c7914f57b5657b5a1f553104b545ef19891ed19
DilmiSeneviratne <dilmiseneviratne@gmail.com>
2024-03-31 15:10:21 +0530

    Homepage completed

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,112 @@
-import React from 'react'
-import './main.css'
+import React from "react";
+import "./main.css";
 
+const Data=[
+  {
+    id:1,
+    imgSrc:img1,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  /*{
+    id:2,
+    imgSrc:img2,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:3,
+    imgSrc:img3,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:4,
+    imgSrc:img4,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:5,
+    imgSrc:img5,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:6,
+    imgSrc:img6,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:7,
+    imgSrc:img7,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:8,
+    imgSrc:img8,
+    destTitle:'Taj Mahal',
+    location:'India',
+    grade:'CULTURAL RELAX',
+    fees:'$900',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id:9,
+    imgSrc:img9,
+    destTitle:'Bora Bora',
+    location:'New Zealand',
+    grade:'CULTURAL RELAX',
+    fees:'$700',
+    description:'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },*/
+
+]
 const Main = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="main container section">
+      <div className="secTitle">
+           <h3 className="title">
+            Most visited destinations
+           </h3>
+      </div>
 
-export default Main
+      <div className="secContent grid">
+
+      </div>
+    </section>
+  );
+};
+
+export default Main;
